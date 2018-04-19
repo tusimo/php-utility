acbcc930f72882a9e474caa097362ad1d9c8523d
tusimo <youchengcheng@hotelgg.com>
2018-04-19 14:52:53 +0800

    add some function support

--- src/helpers.php
+++ src/helpers.php
@@ -230,3 +230,43 @@ if (!function_exists('array_to_tree')) {
         return $tree;
     }
 }
+
+if (!function_exists('object_to_array')) {
+    /**
+     * covert object to array
+     * @param $object
+     * @return mixed
+     */
+    function object_to_array($object)
+    {
+        return json_decode(json_encode($object), true);
+    }
+}
+
+if (!function_exists('string_to_array')) {
+    /**
+     * covert a string to array
+     * @param $string
+     * @param string $option json|JSON as covert a json string to array and else as delimiter for a string to explode
+     * @param null $callback covert array value ,use intval strval floatval boolval and custom callback
+     * @return array|mixed
+     */
+    function string_to_array($string, $option = ',', $callback = null)
+    {
+        $item = [];
+        if ($string != '' && !is_null($string)) {
+            if (strtoupper($option) == 'JSON') {
+                $item = json_decode($string, true);
+                if (!is_array($item)) {
+                    $item = [];
+                }
+            } else {
+                $item = explode($option, $string);
+            }
+        }
+        if ($callback) {
+            return array_map($callback, $item);
+        }
+        return $item;
+    }
+}
